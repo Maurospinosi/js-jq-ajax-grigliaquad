09c332b80fee76ee8a50466752870d86daafe919
Mauro <mauro.spinosi94@gmail.com>
2020-09-16 16:24:11 +0200

    Js parte 1

--- js/menu.js
+++ js/menu.js
@@ -3,5 +3,29 @@
 // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato.
 $(document).ready(function(){
-  
+  $("body").click(function() {
+
+    var endpoint = "https://flynn.boolean.careers/exercises/api/random/int";
+
+    $.ajax(
+     {
+       "url" : endpoint,
+       "method" : "GET",
+       "success" : function (data, stato) {
+         var numRandom = data.response;
+         if(numRandom <= 5){
+           $(".boxes").addClass("style-yellow");
+           $(".boxes").removeClass("style-green");
+           $(".numero").text(numRandom);
+         } else if (numRandom > 5){
+           $(".boxes").addClass("style-green");
+           $(".boxes").removeClass("style-yellow");
+           $(".numero").text(numRandom);
+         }
+       },
+       error: function (richiesta, stato, errori) {
+       }
+     }
+    );
+  });
 });
